202f4571edbd82a3c50c0d17ca647d0da701f44e
Oberdann <132602190+Oberdann@users.noreply.github.com>
2023-08-18 20:51:26 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import "dotenv/config";
 import app from "./scr/app.js";
 
@@ -8,15 +8,3 @@ const port = process.env.PORT || 3000;
 
 app.listen(port, () =>{
   console.log(`Servidor funcionando em: http://localhost:${port}`);
-=======
-import "dotenv/config";
-import app from "./scr/app.js";
-
-
-const port = process.env.PORT || 3000;
-
-
-app.listen(port, () =>{
-  console.log(`Servidor funcionando em: http://localhost:${port}`);
->>>>>>> ac49664c711c9058cbb6e4ee49186ab9145f624b
-});
